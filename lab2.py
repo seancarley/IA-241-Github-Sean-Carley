f275028114f346d6719631625cf71c5f94bf3c3b
EC2 Default User <ec2-user@ip-172-31-91-177.ec2.internal>
2023-01-26 21:57:28 +0000

    lab2.py

---
+++ lab2.py
@@ -0,0 +1,31 @@
+'''
+lab2
+numbers, variables, and strings
+'''
+#6.1
+my_name = 'Tom'
+
+print(my_name.upper())
+#6.2
+my_id = 123
+print(my_id)
+#6.3 Everything on the left is considered a variable, and a variable can't start with a number, so no you cannot define 123 = my_id
+#123 = my_id
+#6.4
+my_id = your_id = 123
+print(my_id)
+print(your_id)
+#6.5
+my_id_str='123'
+print(my_id_str)
+#6.6
+#print(my_name+my_id)
+#6.7
+print(my_name + my_id_str)
+#6.8
+print(my_name * 3)
+#6.9
+print('hello, world. This is my first python string.'.split('.'))
+#6.10
+#message ='Tom's id is 123!!!!!!!''
+message ="Tom's id is 123"

895c3bcfe35fee5a475c3f1067c09deda6af1a18
EC2 Default User <ec2-user@ip-172-31-91-177.ec2.internal>
2023-02-02 22:05:46 +0000

    lab3 and lec3

---
+++ lec3.py
@@ -0,0 +1,56 @@
+'''
+lec 3
+
+list and set
+'''
+
+my_list = [ 1,2,3,4,5 ]
+
+print(type(my_list))
+
+my_nested_list= [1,2,3,my_list ]
+print(my_nested_list)
+
+my_list[0]=6
+print(my_list)
+
+print(my_list[:])
+
+x,y,z = ['a','b','c']
+print(x)
+
+my_list.append(7)
+print(my_list)
+
+my_list.remove(5)
+print(my_list)
+
+my_list.sort()
+print(my_list)
+
+my_list.reverse()
+print(my_list)
+
+print(my_list + [8,9])
+
+my_list.extend([8,9])
+print(my_list)
+
+print( 2 in my_list)
+
+print( '9' in my_list)
+
+print(len(my_nested_list))
+
+print('hello\tworld')
+
+print('hello\nwold')
+
+
+
+
+
+
+
+
+

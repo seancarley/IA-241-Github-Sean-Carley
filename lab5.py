91659bb73ad576d3cdef8a3324ba5ebb9136473c
EC2 Default User <ec2-user@ip-172-31-91-177.ec2.internal>
2023-02-18 02:24:15 +0000

    Lab 5 Submission

---
+++ lab5.py
@@ -0,0 +1,35 @@
+'''
+Lab 5 Sean Carley IA 241
+'''
+#3.1 
+alien_color = 'yellow'
+if alien_color == 'green':
+    print('you got 5 points')
+#3.2
+alien_color = 'pink'
+if alien_color == 'green':
+    print('you got 5 points')
+else:
+    print('you got 10 points')
+    
+#3.3
+favorite_fruits = ['mangos','grapefruit','kiwi']
+
+if 'dragonfruits' in favorite_fruits:
+    print('You really like bananas!')
+if 'mangos' in favorite_fruits:
+    print('You really like bananas!')
+if 'grapes' in favorite_fruits:
+    print('You really like bananas!')
+    
+#3.4
+age = 92
+if age < 10:
+    print('They are a kid.')
+elif age < 20:
+    print('They are a teenager.')
+else:
+    print('They are an adult.')
+    if age > 65:
+        print('They are an elder.')
+    
---
+++ lec5.py
@@ -0,0 +1,32 @@
+import this
+
+print(  1+15+
+            4564)
+            
+1  +  2  +  \
+12+456
+a = [1,2,3]
+b = [1,2,3]
+
+print(id(a))
+print(id(b))
+print(id([1,2,3]))
+
+print( a == b)
+
+print(a is b)
+
+a = None
+
+print(id(a))
+print(id(None))
+
+print(a is None)
+
+x =[]
+print(x is None)
+
+x =None 
+print(x is None)
+
+print( True and False )
